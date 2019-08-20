d544fbf7ebe2b2c0797f41f34c9bf3ea24b09c46
Tarun Prince <tarunprince99@gmail.com>
2019-08-20 16:06:20 -0500

    starting css nav bar error with routing

---
+++ my-app/src/js/nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './css/nav.css'
+
+class Nav(){
+
+<div class="topnav">
+  <a class="active" href="#home">About Me</a>
+  <a class = "resume_link" href="#resume">Resume</a>
+  <a class = "projects_link" href="#projects">Projects/Github</a>
+</div>
+
+}
